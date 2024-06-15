8a27efd7854d59ac3319eea85b0f44f2b0331a60
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-06-15 18:41:40 -0400

    Update expensesReducer.js

--- src/redux/reducers/expensesReducer.js
+++ src/redux/reducers/expensesReducer.js
@@ -2,7 +2,7 @@ import {  CREATE_EXPENSE, EDIT_EXPENSE, DELETE_EXPENSE } from '../actions/expens
 import { v4 as uuidv4 } from 'uuid';
 
 const initialState = {
-  allExpenses: [{"id": 0, "amount": 5, "date": "2024-05-27", "category":"food","description":"coffee"},{"id": 1, "amount": 4, "date": "2024-05-29", "category":"food","description":"donut"}],
+  allExpenses: [{"id": "a376eb46-c302-47e5-9b90-12d0e61ecf79", "amount": 5, "date": "2024-05-27", "category":"food","description":"coffee"},{"id": "c2794c5f-f85d-441c-af76-881310953d9b", "amount": 4, "date": "2024-05-29", "category":"food","description":"donut"}],
   categories: ["Housing", "Food", "Transportation", "Health", "Debt", "Savings", "Personal Care", "Entertainment", "Education", "Charity", "Other"],
 };
 
