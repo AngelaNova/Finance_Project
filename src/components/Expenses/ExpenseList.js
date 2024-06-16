4856ca931fe8005ab9f9df34c434d0ac701f9333
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-06-16 14:20:32 -0400

    Update ExpenseList.js

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -5,12 +5,14 @@ function ExpenseList({expenses}) {
 
   return (
     <div>
-      <h1>Expenses</h1>
-      <ul>
-        {expenses && expenses.map((expense) => (
-          <li key={expense.id}> {expense.description} : {expense.amount} $ | {expense.date} ({expense.category})</li>
-        ))}
-      </ul>
+      <h4 className="text-center">Expenses</h4>
+      <div className="col-12 col-md-8 col-lg-4">
+        <ul>
+          {expenses && expenses.map((expense) => (
+            <li key={expense.id}> {expense.description} : {expense.amount} $ | {expense.date} ({expense.category})</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
