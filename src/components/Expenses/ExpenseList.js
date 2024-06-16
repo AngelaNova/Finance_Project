8ccc6b5861a8495e29637b6865c157d114beb085
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-06-16 14:36:29 -0400

    Update ExpenseList.js

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -4,10 +4,10 @@ function ExpenseList({expenses}) {
 
 
   return (
-    <div>
+    <div class="row justify-content-center">
       <h4 className="text-center">Expenses</h4>
-      <div className="col-12 col-md-8 col-lg-4">
-        <ul>
+      <div className="col-12 mx-auto">
+        <ul className="list-unstyled text-center">
           {expenses && expenses.map((expense) => (
             <li key={expense.id}> {expense.description} : {expense.amount} $ | {expense.date} ({expense.category})</li>
           ))}
