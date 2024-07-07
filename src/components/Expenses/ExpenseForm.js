c02054517b8f40ddf69f9432fd7c5ace3bc4a60e
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-07-07 13:33:00 -0400

    Update ExpenseForm.js

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -107,235 +107,238 @@ const ExpenseForm = () => {
   };
 
   return (
-    <div id="ExpenseManagement" className="container my-5">
+    <>
       <Header />
-      <header className="text-center">
-        <h3>Welcome to the Expenses Menu</h3>
-      </header>
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-8 col-lg-6">
-          <h4 className="text-center">Add an expense below</h4>
 
-          <form onSubmit={handleSubmit}>
-            {" "}
-            {/*onSubmit -> creates a new object to add to the list of expenses */}
-            <div className="mb-3">
-              <label htmlFor="amount" className="form-label">
-                Amount:
-              </label>
-              <input
-                type="number"
-                id="amount"
-                name="amount"
-                className="form-control"
-                ref={(el) => (newExpense.current.amount = el)}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="date" className="form-label">
-                Date:
-              </label>
-              <input
-                type="date"
-                id="date"
-                name="date"
-                className="form-control"
-                ref={(el) => (newExpense.current.date = el)}
-                defaultValue={formatDate()}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="category" className="form-label">
-                Category:
-              </label>
-              <select
-                type="text"
-                id="category"
-                name="category"
-                className="form-select"
-                ref={(el) => (newExpense.current.category = el)}
-                required
-              >
-                <option>Select a category</option>
-                {categories.map((category) => (
-                  <option key={category}>{category}</option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <label htmlFor="description" className="form-label">
-                Description:
-              </label>
-              <input
-                type="text"
-                id="description"
-                name="description"
-                className="form-control"
-                ref={(el) => (newExpense.current.description = el)}
-                required
-              />
-            </div>
-            <button type="submit" className="btn btn-primary mt-4">
-              Add Expense
-            </button>
-          </form>
-        </div>
-      </div>
+      <div id="ExpenseManagement" className="container my-5">
+        <header className="text-center">
+          <h3>Welcome to the Expenses Menu</h3>
+        </header>
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-8 col-lg-6">
+            <h4 className="text-center">Add an expense below</h4>
 
-      <div className="row justify-content-center mt-4 col-16 col-md-8 col-lg-6 mx-auto">
-        <ExpenseFeed expenses={allExpenses} />
-      </div>
-
-      <div className="row justify-content-center mt-4">
-        <div className="col-12 col-md-8 col-lg-6">
-          <h3 className="text-center">Edit Expenses below</h3>
-          <p className="text-center">Input the id of your expense below</p>
-          <div className="mb-3">
-            <input
-              className="form-control"
-              ref={editId}
-              type="text"
-              name="expenseId"
-              id="expenseId"
-              required
-              placeholder="Expense Id"
-            />
+            <form onSubmit={handleSubmit}>
+              {" "}
+              {/*onSubmit -> creates a new object to add to the list of expenses */}
+              <div className="mb-3">
+                <label htmlFor="amount" className="form-label">
+                  Amount:
+                </label>
+                <input
+                  type="number"
+                  id="amount"
+                  name="amount"
+                  className="form-control"
+                  ref={(el) => (newExpense.current.amount = el)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="date" className="form-label">
+                  Date:
+                </label>
+                <input
+                  type="date"
+                  id="date"
+                  name="date"
+                  className="form-control"
+                  ref={(el) => (newExpense.current.date = el)}
+                  defaultValue={formatDate()}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="category" className="form-label">
+                  Category:
+                </label>
+                <select
+                  type="text"
+                  id="category"
+                  name="category"
+                  className="form-select"
+                  ref={(el) => (newExpense.current.category = el)}
+                  required
+                >
+                  <option>Select a category</option>
+                  {categories.map((category) => (
+                    <option key={category}>{category}</option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                <label htmlFor="description" className="form-label">
+                  Description:
+                </label>
+                <input
+                  type="text"
+                  id="description"
+                  name="description"
+                  className="form-control"
+                  ref={(el) => (newExpense.current.description = el)}
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-primary mt-4">
+                Add Expense
+              </button>
+            </form>
           </div>
-          <div className="text-center">
-            <button
-              className="btn btn-secondary mb-3"
-              onClick={() =>
-                setExpenseToEdit(
-                  allExpenses.find(
-                    (expense) => expense.id === editId.current.value
+        </div>
+
+        <div className="row justify-content-center mt-4 col-16 col-md-8 col-lg-6 mx-auto">
+          <ExpenseFeed expenses={allExpenses} />
+        </div>
+
+        <div className="row justify-content-center mt-4">
+          <div className="col-12 col-md-8 col-lg-6">
+            <h3 className="text-center">Edit Expenses below</h3>
+            <p className="text-center">Input the id of your expense below</p>
+            <div className="mb-3">
+              <input
+                className="form-control"
+                ref={editId}
+                type="text"
+                name="expenseId"
+                id="expenseId"
+                required
+                placeholder="Expense Id"
+              />
+            </div>
+            <div className="text-center">
+              <button
+                className="btn btn-secondary mb-3"
+                onClick={() =>
+                  setExpenseToEdit(
+                    allExpenses.find(
+                      (expense) => expense.id === editId.current.value
+                    )
                   )
-                )
-              }
-            >
-              Submit Id
-            </button>
-            <p className="text-center">
-              this is the id you have chosen:{" "}
-              {editId.current.value === undefined ||
-              editId.current.value === null ||
-              !expenseToEdit
-                ? "No expense with such Id found"
-                : editId.current.value}
-            </p>
+                }
+              >
+                Submit Id
+              </button>
+              <p className="text-center">
+                this is the id you have chosen:{" "}
+                {editId.current.value === undefined ||
+                editId.current.value === null ||
+                !expenseToEdit
+                  ? "No expense with such Id found"
+                  : editId.current.value}
+              </p>
+
+              <p>
+                This is the expense you have chosen:{" "}
+                {expenseToEdit ? JSON.stringify(expenseToEdit) : "None"}
+              </p>
+            </div>
 
             <p>
-              This is the expense you have chosen:{" "}
-              {expenseToEdit ? JSON.stringify(expenseToEdit) : "None"}
+              Change the amount of the expense. Input the correct amount below:
             </p>
-          </div>
+            <div className="mb-3">
+              <input
+                ref={newAmount}
+                className="form-control"
+                type="number"
+                id="newAmount"
+                required
+                placeholder="New Amount"
+              />
+            </div>
 
-          <p>
-            Change the amount of the expense. Input the correct amount below:
-          </p>
-          <div className="mb-3">
-            <input
-              ref={newAmount}
-              className="form-control"
-              type="number"
-              id="newAmount"
-              required
-              placeholder="New Amount"
-            />
-          </div>
+            <p>New Amount inputted is : {newAmount.current.value}</p>
+            <div className="text-center">
+              <button
+                className="btn btn-primary mb-3"
+                onClick={handleEditOfAmount}
+              >
+                Submit Edit
+              </button>
+            </div>
 
-          <p>New Amount inputted is : {newAmount.current.value}</p>
-          <div className="text-center">
-            <button
-              className="btn btn-primary mb-3"
-              onClick={handleEditOfAmount}
-            >
-              Submit Edit
-            </button>
-          </div>
+            <p>
+              Change the description of the expense. Input the correct
+              description below:
+            </p>
+            <div className="mb-3">
+              <input
+                ref={newDescription}
+                className="form-control"
+                type="text"
+                id="newDescription"
+                required
+                placeholder="New Description"
+              />
+            </div>
 
-          <p>
-            Change the description of the expense. Input the correct description
-            below:
-          </p>
-          <div className="mb-3">
-            <input
-              ref={newDescription}
-              className="form-control"
-              type="text"
-              id="newDescription"
-              required
-              placeholder="New Description"
-            />
-          </div>
+            <p>New Description inputted is : {newDescription.current.value}</p>
+            <div className="text-center">
+              <button
+                className="btn btn-primary mb-3"
+                onClick={handleEditOfDescription}
+              >
+                Submit Edit
+              </button>
+            </div>
 
-          <p>New Description inputted is : {newDescription.current.value}</p>
-          <div className="text-center">
-            <button
-              className="btn btn-primary mb-3"
-              onClick={handleEditOfDescription}
-            >
-              Submit Edit
-            </button>
-          </div>
-
-          <p className="text-center">
-            Delete an expense. Input the id of the expense to delete it
-          </p>
-          <div className=" mb-3">
-            <input
-              className="form-control"
-              ref={deleteId}
-              required
-              type="text"
-            />
             <p className="text-center">
-              Delete expense with id: {deleteId.current.value}
+              Delete an expense. Input the id of the expense to delete it
             </p>
+            <div className=" mb-3">
+              <input
+                className="form-control"
+                ref={deleteId}
+                required
+                type="text"
+              />
+              <p className="text-center">
+                Delete expense with id: {deleteId.current.value}
+              </p>
+            </div>
+            <div className="text-center">
+              {
+                //make sure to update handleDeleteClick - it doesn't work as intended TODO
+              }
+              <button className="btn btn-danger" onClick={handleDeleteClick}>
+                Delete
+              </button>
+            </div>
+
+            {showDeleteModal && (
+              <DeleteModal
+                deleteId={deleteId}
+                onConfirm={handleDeleteConfirm}
+                onCancel={handleDeleteCancel}
+              />
+            )}
+
+            <h4 className="text-center mt-4">Filter Here</h4>
+
+            <form className="text-center">
+              <select
+                className="form-select"
+                onChange={(event) => true}
+                name="Category"
+                id="category"
+              >
+                <option value=" ">See All</option>
+                {categories.map((category) => (
+                  <option key={category} value={category}>
+                    {" "}
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </form>
+
+            {/*<ExpenseList expenses={filteredExpenses.length > 0 ? filteredExpenses : allExpenses}/>*/}
+
+            <br />
           </div>
-          <div className="text-center">
-            {
-              //make sure to update handleDeleteClick - it doesn't work as intended TODO
-            }
-            <button className="btn btn-danger" onClick={handleDeleteClick}>
-              Delete
-            </button>
-          </div>
-
-          {showDeleteModal && (
-            <DeleteModal
-              deleteId={deleteId}
-              onConfirm={handleDeleteConfirm}
-              onCancel={handleDeleteCancel}
-            />
-          )}
-
-          <h4 className="text-center mt-4">Filter Here</h4>
-
-          <form className="text-center">
-            <select
-              className="form-select"
-              onChange={(event) => true}
-              name="Category"
-              id="category"
-            >
-              <option value=" ">See All</option>
-              {categories.map((category) => (
-                <option key={category} value={category}>
-                  {" "}
-                  {category}
-                </option>
-              ))}
-            </select>
-          </form>
-
-          {/*<ExpenseList expenses={filteredExpenses.length > 0 ? filteredExpenses : allExpenses}/>*/}
-
-          <br />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
