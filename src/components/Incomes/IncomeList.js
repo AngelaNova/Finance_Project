3fc4a214737acb94e24e1f32a458c1c12fd65e12
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-07-07 13:53:39 -0400

    Update IncomeList.js

--- src/components/Incomes/IncomeList.js
+++ src/components/Incomes/IncomeList.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React from "react";
 
-
-function IncomeList({incomes}) {
-
-
+function IncomeList({ incomes }) {
   return (
-    <div>
-      <h1>Incomes</h1>
-      <ul>
-        {incomes.map((income) => (
-          <li key={income.id}>{income.description} : {income.amount} $ | {income.date}</li>
-        ))}
-      </ul>
+    <div className="row justify-content-center mt-4 m-2">
+      <h4 className="text-center">Incomes</h4>
+      <div className="col-12 mr-6 ml-6">
+        {incomes &&
+          incomes.map((income) => (
+            <p
+              className="d-flex justify-content-between align-items-center"
+              key={income.id}
+            >
+              <span className="flex-end">
+                {income.description} : {income.amount} ${" "}
+              </span>
+              <span className="flex-end">
+                {income.date} | <i>{income.category}</i>
+              </span>
+            </p>
+          ))}
+      </div>
     </div>
   );
 }
