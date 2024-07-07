2b420b4e7c7978359e12c8ea7c2e5dc1e752c30b
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-07-07 12:43:52 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import Header from "./Header";
 
 const Home = () => {
-  return;
-  <></>;
+  return (
+    <>
+      <div className="container mt-4">
+        <Header />
+        <main>
+          <section className="text-center mt-5">
+            <h2>Welcome to Financial App</h2>
+            <p>
+              Track your expenses, income, and get a financial summary
+              effortlessly.
+            </p>
+          </section>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Home;
