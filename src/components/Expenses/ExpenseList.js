70ef69d134a44cb720775b3cbb812e6b341b7302
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-07-07 15:50:48 -0400

    Update ExpenseList.js

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
 
-function ExpenseList({expenses}) {
-
-
+function ExpenseList({ expenses }) {
   return (
     <div className="row justify-content-center mt-4 m-2">
       <h4 className="text-center">Expenses</h4>
       <div className="col-12 mr-6 ml-6">
-
-          {expenses && expenses.map((expense) => (
-            <p className="d-flex justify-content-between align-items-center" key={expense.id}> 
-              <span className="flex-end">{expense.description} : {expense.amount} $ </span>
-              <span className="flex-end">{expense.date} | <i>{expense.category}</i></span>
-            </p>
+        {expenses &&
+          expenses.map((expense) => (
+            <div className="">
+              <p
+                className="d-flex justify-content-between align-items-center"
+                key={expense.id}
+              >
+                <span className="flex-end">
+                  {expense.description} : {expense.amount} ${" "}
+                </span>
+                <span className="flex-end">
+                  {expense.date} | <i>{expense.category}</i>
+                </span>
+              </p>
+            </div>
           ))}
       </div>
     </div>
