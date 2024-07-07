28d5c1ec7f599d65390808fec5ad86df8cc25b5c
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-07-07 12:43:49 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import ExpenseForm from './components/Expenses/ExpenseForm';
-import IncomeForm from './components/Incomes/IncomeForm';
-import FinancialSummary from './components/FinancialSummary/FinancialSummary';
-import Header from './components/Header';
-
-
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import ExpenseForm from "./components/Expenses/ExpenseForm";
+import IncomeForm from "./components/Incomes/IncomeForm";
+import FinancialSummary from "./components/FinancialSummary/FinancialSummary";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <Header/>
-        <ExpenseForm/>
-        <IncomeForm/>
-        <FinancialSummary/>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/expense-management" element={<ExpenseForm />} />
+          <Route path="/income-management" element={<IncomeForm />} />
+          <Route path="/financial-summary" element={<FinancialSummary />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       </Provider>
     </BrowserRouter>
   );
